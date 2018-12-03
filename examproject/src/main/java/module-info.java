d757b3fd60c354ea2c1b1f6186c1ef947d7dc1b3
shayneh58 <shayneh58@BHSCUR149007ITW.kprdsb.schools.local>
2018-12-03 14:02:12 -0500

    First Commit

---
+++ examproject/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module humphriesmartinfice {requires javafx.fxml; 
+requires javafx.controls; 
+requires javafx.graphics; 
+requires javafx.base; 
+requires javafx.media; 
+opens humphriesmartinfice.examproject;}
