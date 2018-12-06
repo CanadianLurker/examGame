dd2b49b8fb0d2a69c92b3320b6bb35bd1e4eb382
n00bi <n00bi@DESKTOP-7HKUNVF>
2018-12-06 13:40:26 -0500

    background

--- examproject/src/main/java/humphriesmartinfice/examproject/FXMLController.java
+++ examproject/src/main/java/humphriesmartinfice/examproject/FXMLController.java
@@ -6,16 +6,28 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 public class FXMLController implements Initializable {
     
     @FXML
     private Label label;
+    @FXML
+    private ListView savesList;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void btnLoad(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    private void btnOptions(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    private void btnExit(ActionEvent event) {
+        
     }
     
     @Override
