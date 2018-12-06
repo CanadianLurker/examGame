3aefe0cbcf8abdcc90d23a20f45279e7b6eb2d97
n00bi <n00bi@DESKTOP-7HKUNVF>
2018-12-06 14:04:05 -0500

    added spawn room

---
+++ examproject/src/main/java/humphriesmartinfice/examproject/FXMLStartController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package humphriesmartinfice.examproject;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+/**
+ * FXML Controller class
+ *
+ * @author n00bi
+ */
+public class FXMLStartController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
