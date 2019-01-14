ee62f26e0d3df37c35e826a202dacb9bab5ad732
Shayne <Shayne@192.168.2.10>
2019-01-13 22:38:46 -0500

    Block A now under and nearly finished construction, also changed and fixed some other things

--- examproject/src/main/java/humphriesmartinfice/examproject/MainApp.java
+++ examproject/src/main/java/humphriesmartinfice/examproject/MainApp.java
@@ -1,7 +1,6 @@
 package humphriesmartinfice.examproject;
 
-import humphriesmartinfice.examproject.MainApp;
-import java.lang.reflect.Array;
+import java.io.IOException;
 import java.util.ArrayList;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
@@ -12,17 +11,19 @@ import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
-    public static int level, STR, DEX, INT, edot;
-    
+    public static int level, STR, DEX, INT, edot, cigs;
+
     public static int ecount = 0;
-    
+
     public static boolean dot = false;
 
-    public static double health, healthMAX, mana, manaMAX, EXP, EXPNeeded;
+    public static double health, healthMAX, mana, manaMAX, EXP, EXPNeeded, layX, layY;
+
+    public static Parent area;
 
     public static ArrayList<Enemy> enemies = new ArrayList();
 
-    public static Weapon weapon = new Mage(50, "", "", "", "", 0, 0, 0, 0, "", "");
+    public static Weapon weapon = new Mage(1, "", "", "", "", 0, 0, 0, 0, "", "");
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -36,8 +37,34 @@ public class MainApp extends Application {
         stage.setOnCloseRequest(e -> System.exit(0));
     }
 
+    public static void saveLoc(Parent a, double x, double y){
+        area = a;
+        layX = x;
+        layY = y;
+    }
+    
+    public static double getLocX(){
+    return layX; 
+    }
+    
+    public static double getLocY(){
+    return layY;
+    }
+    
+    public static Parent getArea(){
+    return area;
+    }
+
     public static int getSTR() {
         return MainApp.STR;
+    }
+
+    public static int getCigs() {
+        return cigs;
+    }
+
+    public static void setCigs(int c) {
+        cigs = c;
     }
 
     public static void setSTR(int point) {
@@ -47,21 +74,21 @@ public class MainApp extends Application {
     public static int getDEX() {
         return MainApp.DEX;
     }
-    
-    public static void setEcount(int E){
-    ecount = E;
+
+    public static void setEcount(int E) {
+        ecount = E;
     }
-    
-    public static int getEcount(){
-    return ecount;
+
+    public static int getEcount() {
+        return ecount;
     }
-    
-    public static void setEdot(int E){
-    edot = E;
+
+    public static void setEdot(int E) {
+        edot = E;
     }
-    
-    public static int getEdot(){
-    return edot;
+
+    public static int getEdot() {
+        return edot;
     }
 
     public static void setDEX(int point) {
@@ -93,7 +120,7 @@ public class MainApp extends Application {
     }
 
     public static void setHealthMAX() {
-        MainApp.healthMAX = MainApp.level * 10 + (MainApp.STR * 5) + (MainApp.DEX * 2);
+        MainApp.healthMAX = MainApp.level * 10 + (MainApp.STR * 5) + (MainApp.DEX * 2) + (MainApp.INT);
     }
 
     public static double getHealthMAX() {
@@ -113,7 +140,7 @@ public class MainApp extends Application {
     }
 
     public static void setManaMAX() {
-        MainApp.manaMAX = MainApp.level * 3 + 4 + (MainApp.INT * 2) + MainApp.DEX;
+        MainApp.manaMAX = MainApp.level * 3 + 4 + (MainApp.INT * 3) + (MainApp.DEX * 2) + MainApp.INT;
     }
 
     public static double getEXP() {
@@ -129,7 +156,7 @@ public class MainApp extends Application {
     }
 
     public static void setEXPNeeded() {
-        MainApp.EXPNeeded = 22 + (MainApp.level * 12);
+        MainApp.EXPNeeded = 25 + (MainApp.level * 18);
     }
 
     /**
